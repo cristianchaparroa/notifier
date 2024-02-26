64c013e08438a89b3c083e0527efa19307c2486c
cristianchaparroa <cristianchaparroa@gmail.com>
2024-02-26 02:44:40 -0500

    configure the smpt server to sent emails

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/stretchr/testify v1.8.4
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
@@ -22,6 +23,7 @@ require (
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- internal/usecase/email.go
+++ internal/usecase/email.go
@@ -1,15 +1,47 @@
 package usecase
 
-import "fmt"
+import (
+	"crypto/tls"
+	"fmt"
+	"gopkg.in/gomail.v2"
+	"notifier/config"
+)
 
-type emailManager struct {
+type noopManager struct{}
+
+func (m *noopManager) Send(subject, addressee, body string) error {
+	fmt.Printf("--> Mock Email Manager - subject %s, Receipent:%s, content:%s\n", subject, addressee, body)
+	return nil
 }
 
-func NewEmailManager() EmailManager {
-	return &emailManager{}
+type emailManager struct {
+	cfg *config.Config
+}
+
+func NewEmailManager(cfg *config.Config) EmailManager {
+	if config.EnvProduction == cfg.App.Env {
+		return &emailManager{
+			cfg: cfg,
+		}
+	}
+	return &noopManager{}
 }
 
 func (m *emailManager) Send(subject, addressee, body string) error {
-	fmt.Printf("--> Mock Email Manager - subject %s, Receipent:%s, content:%s\n", subject, addressee, body)
+	fmt.Printf("--> Real Email Manager - subject %s, Receipent:%s, content:%s\n", subject, addressee, body)
+	fmt.Println(m.cfg.SMTP)
+	message := gomail.NewMessage()
+	message.SetHeader("From", m.cfg.SMTP.User)
+	message.SetHeader("To", addressee)
+	message.SetHeader("Subject", subject)
+	message.SetBody("text", body)
+
+	d := gomail.NewDialer(m.cfg.SMTP.Server, m.cfg.SMTP.Port, m.cfg.SMTP.User, m.cfg.SMTP.Password)
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	if err := d.DialAndSend(message); err != nil {
+		return err
+	}
+
+	return nil
 	return nil
 }
